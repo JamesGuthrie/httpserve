04239a95ac7f8d0dd959ad87ddb7802da3ed1f2c
James Guthrie <james@guthrie.ch>
2026-05-27 15:02:13 +0200

    chore: bump hyper to 1.9.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use clap::{crate_version, App, Arg};
+use http_body_util::combinators::BoxBody;
+use http_body_util::{BodyExt, Empty, Full};
+use hyper::body::{Bytes, Incoming};
 use hyper::header::LOCATION;
-use hyper::http::uri::Builder;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::http::uri::Builder as UriBuilder;
+use hyper::service::service_fn;
+use hyper::{Method, Request, Response, StatusCode};
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use hyper_util::server::conn::auto::Builder;
+use hyper_util::server::graceful::GracefulShutdown;
 use log::{debug, error, info, warn};
 use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 use std::collections::{HashMap, VecDeque};
@@ -11,7 +17,10 @@ use std::fs;
 use std::fs::read;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::PathBuf;
+use std::pin::pin;
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
@@ -25,26 +34,50 @@ async fn main() {
         config.redirect_http,
     ));
 
-    let make_svc = make_service_fn(move |_conn| {
-        let file_server = Arc::clone(&file_server);
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
+    let listener = TcpListener::bind(&addr)
+        .await
+        .unwrap_or_else(|_| panic!("unable to bind {}:{}", addr.ip(), addr.port()));
+
+    let server = Builder::new(TokioExecutor::new());
+
+    let graceful = GracefulShutdown::new();
+
+    let mut ctrl_c = pin!(tokio::signal::ctrl_c());
+
+    loop {
+        tokio::select! {
+            Ok((stream, peer_addr)) = listener.accept() => {
+                let stream = TokioIo::new(Box::pin(stream));
                 let file_server = Arc::clone(&file_server);
-                async move { file_server.handle(req).await }
-            }))
+                let conn = server.serve_connection_with_upgrades(stream, service_fn(move |req| {
+                   let file_server = Arc::clone(&file_server);
+                    async move { file_server.handle(req).await }
+                }));
+
+                let conn = graceful.watch(conn.into_owned());
+
+                tokio::spawn(async move {
+                    if let Err(err) = conn.await {
+                        error!("connection error: {}", err);
+                    }
+                    warn!("connection dropped: {}", peer_addr);
+                });
+            },
+            _ = ctrl_c.as_mut() => {
+                drop(listener);
+                info!("graceful shutdown request received");
+                break;
+            }
         }
-    });
+    }
 
-    let server = Server::bind(&addr)
-        .serve(make_svc)
-        .with_graceful_shutdown(async {
-            tokio::signal::ctrl_c()
-                .await
-                .expect("failed to install CTRL+C signal handler")
-        });
-
-    if let Err(e) = server.await {
-        error!("server error: {}", e);
+    tokio::select! {
+        _ = graceful.shutdown() => {
+            info!("server shutdown gracefully");
+        },
+        _ = tokio::time::sleep(Duration::from_secs(10)) => {
+            warn!("timed out waiting for connections to close");
+        }
     }
 }
 
@@ -131,7 +164,7 @@ impl FileServer {
     pub fn new(dir: PathBuf, http_to_https_redirect: bool) -> FileServer {
         let mut cache: HashMap<String, Vec<u8>> = HashMap::new();
         let mut to_visit: VecDeque<PathBuf> = VecDeque::from(vec![dir.clone()]);
-        while to_visit.len() > 0 {
+        while !to_visit.is_empty() {
             match to_visit.pop_front() {
                 Some(item) => {
                     if item.is_dir() {
@@ -144,7 +177,7 @@ impl FileServer {
                         let copy = item.to_owned();
                         let file_path = copy.to_str().expect("Path not Unicode");
                         let path = file_path
-                            .strip_prefix((*&dir).to_str().expect("Path not Unicode"))
+                            .strip_prefix(dir.to_str().expect("Path not Unicode"))
                             .unwrap();
                         let content = read(item).expect("Failed to read file");
                         debug!("Loaded {} bytes from {}", content.len(), path);
@@ -156,13 +189,16 @@ impl FileServer {
                 }
             }
         }
-        return FileServer {
+        FileServer {
             cache,
             http_to_https_redirect,
-        };
+        }
     }
 
-    async fn handle(&self, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    async fn handle(
+        &self,
+        req: Request<Incoming>,
+    ) -> Result<Response<BoxBody<Bytes, Infallible>>, Infallible> {
         let method = req.method();
         let uri = req.uri();
 
@@ -179,39 +215,40 @@ impl FileServer {
                         }
                     }
                     let maybe_body = self.cache.get(&*path);
-                    return match maybe_body {
+                    match maybe_body {
                         Some(body) => Response::builder()
                             .status(StatusCode::OK)
-                            .body(Body::from(body.to_owned()))
+                            .body(Full::new(Bytes::from(body.to_owned())).boxed())
                             .expect("Unable to create `http::Response`"),
                         None => Response::builder()
                             .status(StatusCode::NOT_FOUND)
-                            .body(Body::empty())
+                            .body(Empty::new().boxed())
                             .expect("Unable to create `http::Response`"),
-                    };
+                    }
                 })
             }
             _ => Response::builder()
                 .status(StatusCode::METHOD_NOT_ALLOWED)
-                .body(Body::empty())
+                .body(Empty::new().boxed())
                 .expect("Unable to create `http::Response`"),
         };
-        return Ok(response);
+        Ok(response)
     }
 
     /// A simple http -> https redirect, based on the presence of the `x-forwarded-proto` header in
     /// the request. This is as described in the following fly.io blog post:
     /// https://fly.io/blog/always-be-connecting-with-https/
-    fn build_https_redirect(&self, req: &Request<Body>) -> Option<Response<Body>> {
+    fn build_https_redirect(
+        &self,
+        req: &Request<Incoming>,
+    ) -> Option<Response<BoxBody<Bytes, Infallible>>> {
         let uri = req.uri();
         if !self.http_to_https_redirect {
             return None;
         }
 
         let fwd_proto = req.headers().get("x-forwarded-proto");
-        if !fwd_proto.is_some() {
-            return None;
-        }
+        fwd_proto?;
 
         if fwd_proto.unwrap() != "http" {
             return None;
@@ -229,7 +266,7 @@ impl FileServer {
             |v| v.to_str().unwrap(),
         );
 
-        let https_request = Builder::new()
+        let https_request = UriBuilder::new()
             .scheme("https")
             .path_and_query(path_and_query.clone())
             .authority(host)
@@ -238,12 +275,12 @@ impl FileServer {
 
         info!("Redirecting to https for {}", path_and_query);
 
-        return Some(
+        Some(
             Response::builder()
                 .status(StatusCode::MOVED_PERMANENTLY)
                 .header(LOCATION, https_request.to_string())
-                .body(Body::empty())
+                .body(Empty::new().boxed())
                 .expect("Unable to create https redirect"),
-        );
+        )
     }
 }
